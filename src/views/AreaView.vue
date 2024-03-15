6d62881cb8daaf903ab1af314baaf9077bdf22e1
liuyinliang <1176440019@qq.com>
2024-03-15 14:28:08 +0800

    feat: 拖拽选区不限制大小

--- src/views/AreaView.vue
+++ src/views/AreaView.vue
@@ -25,11 +25,9 @@ const resizePosition = reactive<ResizePosition[]>([
   'right-bottom'
 ])
 const positionType = ref<ResizePosition>('')
-const maxMinSize = reactive({
-  maxWidth: 0,
-  maxHeight: 0,
-  minWidth: 10,
-  minHeigt: 10
+const maxSize = reactive({
+  width: 0,
+  height: 0
 })
 
 const isMove = ref<boolean>(false)
@@ -49,43 +47,43 @@ const onMousedown = (event: any, position: ResizePosition) => {
   positionType.value = position
   switch (position) {
     case 'left': {
-      maxMinSize.maxWidth = Math.trunc(rect.right - boxRect.left)
-      maxMinSize.maxHeight = rect.height
+      maxSize.width = Math.trunc(rect.right - boxRect.left)
+      maxSize.height = rect.height
       break
     }
     case 'right': {
-      maxMinSize.maxWidth = Math.trunc(boxRect.right - rect.left)
-      maxMinSize.maxHeight = rect.height
+      maxSize.width = Math.trunc(boxRect.right - rect.left)
+      maxSize.height = rect.height
       break
     }
     case 'top': {
-      maxMinSize.maxWidth = rect.width
-      maxMinSize.maxHeight = Math.trunc(rect.bottom - boxRect.top)
+      maxSize.width = rect.width
+      maxSize.height = Math.trunc(rect.bottom - boxRect.top)
       break
     }
     case 'bottom': {
-      maxMinSize.maxWidth = rect.width
-      maxMinSize.maxHeight = Math.trunc(boxRect.bottom - rect.top)
+      maxSize.width = rect.width
+      maxSize.height = Math.trunc(boxRect.bottom - rect.top)
       break
     }
     case 'left-top': {
-      maxMinSize.maxWidth = Math.trunc(rect.right - boxRect.left)
-      maxMinSize.maxHeight = Math.trunc(rect.bottom - boxRect.top)
+      maxSize.width = Math.trunc(rect.right - boxRect.left)
+      maxSize.height = Math.trunc(rect.bottom - boxRect.top)
       break
     }
     case 'left-bottom': {
-      maxMinSize.maxWidth = Math.trunc(rect.right - boxRect.left)
-      maxMinSize.maxHeight = Math.trunc(boxRect.bottom - rect.top)
+      maxSize.width = Math.trunc(rect.right - boxRect.left)
+      maxSize.height = Math.trunc(boxRect.bottom - rect.top)
       break
     }
     case 'right-top': {
-      maxMinSize.maxWidth = Math.trunc(boxRect.right - rect.left)
-      maxMinSize.maxHeight = Math.trunc(rect.bottom - boxRect.top)
+      maxSize.width = Math.trunc(boxRect.right - rect.left)
+      maxSize.height = Math.trunc(rect.bottom - boxRect.top)
       break
     }
     case 'right-bottom': {
-      maxMinSize.maxWidth = Math.trunc(boxRect.right - rect.left)
-      maxMinSize.maxHeight = Math.trunc(boxRect.bottom - rect.top)
+      maxSize.width = Math.trunc(boxRect.right - rect.left)
+      maxSize.height = Math.trunc(boxRect.bottom - rect.top)
       break
     }
   }
@@ -99,79 +97,127 @@ const onMousemove = (event: any) => {
     switch (positionType.value) {
       case 'left': {
         const width = Math.trunc(rect.right - mouseX)
-        if (width <= maxMinSize.maxWidth && width >= maxMinSize.minWidth) {
+        if (width <= maxSize.width) {
           areaRef.value.style.width = width + 'px'
-          areaRef.value.style.left = mouseX - boxRect.left + 'px'
+          width > 0 && (areaRef.value.style.left = mouseX - boxRect.left + 'px')
+        }
+        if (width < 0) {
+          positionType.value = 'right'
         }
         break
       }
       case 'right': {
         const width = Math.trunc(mouseX - rect.left)
-        if (width <= maxMinSize.maxWidth && width >= maxMinSize.minWidth) {
+        if (width <= maxSize.width) {
           areaRef.value.style.width = width + 'px'
+        }
+        if (width < 0) {
+          positionType.value = 'left'
         }
         break
       }
       case 'top': {
         const height = Math.trunc(rect.bottom - mouseY)
-        if (height <= maxMinSize.maxHeight && height >= maxMinSize.minHeigt) {
+        if (height <= maxSize.height) {
           areaRef.value.style.height = height + 'px'
-          areaRef.value.style.top = mouseY - boxRect.top + 'px'
+          height > 0 && (areaRef.value.style.top = mouseY - boxRect.top + 'px')
+        }
+        if (height < 0) {
+          positionType.value = 'bottom'
         }
         break
       }
       case 'bottom': {
         const height = Math.trunc(mouseY - rect.top)
-        if (height <= maxMinSize.maxHeight && height >= maxMinSize.minHeigt) {
+        if (height <= maxSize.height) {
           areaRef.value.style.height = height + 'px'
+        }
+        if (height < 0) {
+          positionType.value = 'top'
         }
         break
       }
       case 'left-top': {
         const width = Math.trunc(rect.right - mouseX)
         const height = Math.trunc(rect.bottom - mouseY)
-        if (width <= maxMinSize.maxWidth && width >= maxMinSize.minWidth) {
+        if (width <= maxSize.width) {
           areaRef.value.style.width = width + 'px'
-          areaRef.value.style.left = mouseX - boxRect.left + 'px'
+          width > 0 && (areaRef.value.style.left = mouseX - boxRect.left + 'px')
         }
-        if (height <= maxMinSize.maxHeight && height >= maxMinSize.minHeigt) {
+        if (height <= maxSize.height) {
           areaRef.value.style.height = height + 'px'
-          areaRef.value.style.top = mouseY - boxRect.top + 'px'
+          height > 0 && (areaRef.value.style.top = mouseY - boxRect.top + 'px')
+        }
+        if (width < 0 && height > 0) {
+          positionType.value = 'right-top'
+        }
+        if (width < 0 && height < 0) {
+          positionType.value = 'right-bottom'
+        }
+        if (width > 0 && height < 0) {
+          positionType.value = 'left-bottom'
         }
         break
       }
       case 'left-bottom': {
         const width = Math.trunc(rect.right - mouseX)
         const height = Math.trunc(mouseY - rect.top)
-        if (width <= maxMinSize.maxWidth && width >= maxMinSize.minWidth) {
+        if (width <= maxSize.width) {
           areaRef.value.style.width = width + 'px'
-          areaRef.value.style.left = mouseX - boxRect.left + 'px'
+          width > 0 && (areaRef.value.style.left = mouseX - boxRect.left + 'px')
         }
-        if (height <= maxMinSize.maxHeight && height >= maxMinSize.minHeigt) {
+        if (height <= maxSize.height) {
           areaRef.value.style.height = height + 'px'
+        }
+        if (width < 0 && height > 0) {
+          positionType.value = 'right-bottom'
+        }
+        if (width < 0 && height < 0) {
+          positionType.value = 'right-top'
+        }
+        if (width > 0 && height < 0) {
+          positionType.value = 'left-top'
         }
         break
       }
       case 'right-top': {
         const width = Math.trunc(mouseX - rect.left)
         const height = Math.trunc(rect.bottom - mouseY)
-        if (width <= maxMinSize.maxWidth && width >= maxMinSize.minWidth) {
+        if (width <= maxSize.width) {
           areaRef.value.style.width = width + 'px'
         }
-        if (height <= maxMinSize.maxHeight && height >= maxMinSize.minHeigt) {
+        if (height <= maxSize.height) {
           areaRef.value.style.height = height + 'px'
-          areaRef.value.style.top = mouseY - boxRect.top + 'px'
+          height > 0 && (areaRef.value.style.top = mouseY - boxRect.top + 'px')
+        }
+        if (width < 0 && height > 0) {
+          positionType.value = 'left-top'
+        }
+        if (width < 0 && height < 0) {
+          positionType.value = 'left-bottom'
+        }
+        if (width > 0 && height < 0) {
+          positionType.value = 'right-bottom'
         }
         break
       }
       case 'right-bottom': {
         const width = Math.trunc(mouseX - rect.left)
         const height = Math.trunc(mouseY - rect.top)
-        if (width <= maxMinSize.maxWidth && width >= maxMinSize.minWidth) {
+        if (width <= maxSize.width) {
           areaRef.value.style.width = width + 'px'
         }
-        if (height <= maxMinSize.maxHeight && height >= maxMinSize.minHeigt) {
+        if (height <= maxSize.height) {
           areaRef.value.style.height = height + 'px'
+        }
+        if (width < 0 && height > 0) {
+          positionType.value = 'left-bottom'
+        }
+        if (width < 0 && height < 0) {
+          positionType.value = 'left-top'
+        }
+        if (width > 0 && height < 0) {
+          positionType.value = 'right-top'
         }
         break
       }
